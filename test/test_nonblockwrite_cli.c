83ead8142e62fc3c4487d501479f498175ef1ee8
3yude <lowfhon@outlook.com>
2023-02-28 16:08:29 +0800

    test: test nonblock read

---
+++ test/test_nonblockwrite_cli.c
@@ -0,0 +1,67 @@
+#include "net.h"
+#include <unistd.h>
+
+void echo(int socketfd);
+
+int main(int argc, char **argv)
+{
+    /* int					sockfd; */
+    struct sockaddr_in	servaddr;
+
+
+    int sockfd = socket(PF_INET, SOCK_STREAM, 0);         
+    /* sockfd = socket(AF_INET, SOCK_STREAM, 0); */
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(SERV_PORT);
+
+    inet_pton(AF_INET, SERV_IP, &servaddr.sin_addr);
+
+    int flag = connect(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr));
+
+    int val, maxdfp1;
+    fd_set rset;
+    
+    val = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, val | O_NONBLOCK);
+
+    /* FD_ZERO(&rset); */
+    /* FD_SET(STDERR_FILENO, &rset); */
+    /* FD_SET(sockfd, &rset); */
+    
+
+
+    /* maxdfp1 = MAX(fileno(stdin),sockfd) + 1; */
+
+    /* printf("Before select\n"); */
+
+    /* select(maxdfp1, &rset, 0, 0, 0); */
+
+    /* printf("After select\n"); */
+
+    /* fflush(stdout); */
+
+    echo(sockfd);
+
+    close(sockfd);
+
+    return 0;
+}
+void echo(int socketfd) {
+    const int maxbuf = 1024;
+    char buf[maxbuf];
+    for (;;) {
+        int n;
+        if ((n = read(STDIN_FILENO, buf, maxbuf)) < 0) {
+            if (errno != EWOULDBLOCK)
+                printf("Client: Error read!\n");
+        } else if (n == 0) {
+            printf("Client: Finish\n");
+            break;
+        } else {
+            write(socketfd, buf, n);
+        }
+        printf("IN FOR\n");
+    }
+}
---
+++ test/test_nonblockwrite_server.c
@@ -0,0 +1,61 @@
+#include "net.h"
+
+void sendfile(int connfd);
+void echo(int);
+
+int main(int argc, char **argv) {
+    int listenfd, connfd;
+    pid_t childpid;
+    socklen_t clilen;
+    struct sockaddr_in cliaddr, servaddr;
+
+    /* AF_INET --> IPv4 , SOCK_STREAM --> tcp, protocol = 0 --> will select corresponding protocol automactically */
+    listenfd = socket(PF_INET, SOCK_STREAM, 0);
+
+    /* init servaddr */
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);  /* accept all address */
+    servaddr.sin_port = htons(SERV_PORT);
+    
+    const int reuse;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEPORT, &reuse, sizeof(reuse));
+    int bindret = bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+
+    listen(listenfd, LISTENQ);
+
+    for(;;) {
+        clilen = sizeof(cliaddr);
+        connfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen);
+        /* if ((childpid = fork()) == 0) { */
+        /*     close(listenfd); */
+
+            /* do something in child process */
+        printf("Server: Before echo");
+        fflush(stdout);
+        /* echo(connfd); */
+        printf("Server: After echo");
+
+            /* exit(0); */
+        /* } */
+        close(connfd);
+    }
+}
+
+/* void echo(int socketfd) { */
+/*     const int maxbuf = 1024; */
+/*     char buf[maxbuf]; */
+/*     for (;;) { */
+/*         int n; */
+/*         if ((n = read(socketfd, buf, maxbuf)) < 0) { */
+/*             printf("Error read!"); */
+/*             exit(1); */
+/*         } else if (n == 0) { */
+/*             printf("Error read!"); */
+/*             break; */
+/*         } else { */
+/*             write(fileno(stdout), buf, n); */
+/*         } */
+/*     } */
+/* } */
